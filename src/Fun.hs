998fec4f2825acf57583969ffcc9b2e775b2df7b
drew <drew@drewolson.org>
2022-01-08 08:56:43 -0600

    No type hint needed

--- src/Fun.hs
+++ src/Fun.hs
@@ -15,7 +15,7 @@ type Counter = TVar Integer
 runRequestCounterIO :: (IOE :> es) => Counter -> Eff (RequestCounter : es) a -> Eff es a
 runRequestCounterIO counter = interpret $ const \case
   CurrentCount -> UIO.atomically $ UIO.readTVar counter
-  IncrementCount -> UIO.atomically $ UIO.modifyTVar counter (+ (1 :: Integer))
+  IncrementCount -> UIO.atomically $ UIO.modifyTVar counter (+ 1)
 
 runIO :: Counter -> Eff '[RequestCounter, IOE] a -> IO a
 runIO counter = runEff . runRequestCounterIO counter
