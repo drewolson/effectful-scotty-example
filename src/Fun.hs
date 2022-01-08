6f681b83da121bfcb78e2250ebd78a3dca8e438c
drew <drew@drewolson.org>
2022-01-08 13:27:48 -0600

    Use Concurrent effect

--- src/Fun.hs
+++ src/Fun.hs
@@ -4,21 +4,22 @@ module Fun
 where
 
 import Effectful (Eff, IOE, interpret, runEff, type (:>))
+import Effectful.Concurrent.STM (Concurrent, TVar)
+import Effectful.Concurrent.STM qualified as STM
 import Fun.RequestCounter (RequestCounter (..))
 import Fun.Router qualified as Router
-import UnliftIO (TVar)
 import UnliftIO qualified as UIO
 import Web.Scotty.Trans (scottyT)
 
 type Counter = TVar Integer
 
-runRequestCounterIO :: (IOE :> es) => Counter -> Eff (RequestCounter : es) a -> Eff es a
+runRequestCounterIO :: (Concurrent :> es) => Counter -> Eff (RequestCounter : es) a -> Eff es a
 runRequestCounterIO counter = interpret $ const \case
-  CurrentCount -> UIO.readTVarIO counter
-  IncrementCount -> UIO.atomically $ UIO.modifyTVar counter (+ 1)
+  CurrentCount -> STM.readTVarIO counter
+  IncrementCount -> STM.atomically $ STM.modifyTVar counter (+ 1)
 
-runIO :: Counter -> Eff '[RequestCounter, IOE] a -> IO a
-runIO counter = runEff . runRequestCounterIO counter
+runIO :: Counter -> Eff '[RequestCounter, Concurrent, IOE] a -> IO a
+runIO counter = runEff . STM.runConcurrent . runRequestCounterIO counter
 
 main :: IO ()
 main = do
