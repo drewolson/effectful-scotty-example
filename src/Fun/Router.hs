8d26aa641e1470a2441be8ce7b741533026e6fdb
drew <drew@drewolson.org>
2022-01-08 08:41:34 -0600

    Use IOE rather than MonadIO

--- src/Fun/Router.hs
+++ src/Fun/Router.hs
@@ -3,16 +3,15 @@ module Fun.Router
   )
 where
 
-import Control.Monad.IO.Class (MonadIO)
 import Control.Monad.Trans.Class (lift)
 import Data.Text.Lazy (Text)
 import Data.Text.Lazy qualified as Text
-import Effectful (Eff, type (:>))
+import Effectful (Eff, IOE, type (:>))
 import Fun.RequestCounter (RequestCounter (..))
 import Fun.RequestCounter qualified as RequestCounter
 import Web.Scotty.Trans (ScottyT, get, html)
 
-router :: (MonadIO (Eff es), RequestCounter :> es) => ScottyT Text (Eff es) ()
+router :: (RequestCounter :> es, IOE :> es) => ScottyT Text (Eff es) ()
 router = do
   get "/" do
     lift RequestCounter.incrementCount
