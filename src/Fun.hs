a17e5d57ca12e28237bf1770e68b0a2c3d1d2ff1
drew <drew@drewolson.org>
2022-01-08 08:55:05 -0600

    No Reader needed

--- src/Fun.hs
+++ src/Fun.hs
@@ -4,8 +4,6 @@ module Fun
 where
 
 import Effectful (Eff, IOE, interpret, runEff, type (:>))
-import Effectful.Reader (Reader)
-import Effectful.Reader qualified as Reader
 import Fun.RequestCounter (RequestCounter (..))
 import Fun.Router as Router
 import UnliftIO (TVar)
@@ -14,17 +12,13 @@ import Web.Scotty.Trans (scottyT)
 
 type Counter = TVar Integer
 
-runRequestCounterIO :: (Reader Counter :> es, IOE :> es) => Eff (RequestCounter : es) a -> Eff es a
-runRequestCounterIO = interpret $ const \case
-  CurrentCount -> do
-    counter <- Reader.ask
-    UIO.atomically $ UIO.readTVar counter
-  IncrementCount -> do
-    counter <- Reader.ask
-    UIO.atomically $ UIO.modifyTVar counter (+ (1 :: Integer))
+runRequestCounterIO :: (IOE :> es) => Counter -> Eff (RequestCounter : es) a -> Eff es a
+runRequestCounterIO counter = interpret $ const \case
+  CurrentCount -> UIO.atomically $ UIO.readTVar counter
+  IncrementCount -> UIO.atomically $ UIO.modifyTVar counter (+ (1 :: Integer))
 
-runIO :: Counter -> Eff '[RequestCounter, Reader Counter, IOE] a -> IO a
-runIO counter = runEff . Reader.runReader counter . runRequestCounterIO
+runIO :: Counter -> Eff '[RequestCounter, IOE] a -> IO a
+runIO counter = runEff . runRequestCounterIO counter
 
 main :: IO ()
 main = do
