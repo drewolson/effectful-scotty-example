c7b15933e9b7ab1fed79cd23833af20955d14e90
drew <drew@drewolson.org>
2022-07-16 09:08:25 -0500

    Reinterpret RequestCounter via Concurrent

--- src/Fun.hs
+++ src/Fun.hs
@@ -4,9 +4,9 @@ module Fun
 where
 
 import Effectful (Eff, IOE, runEff, type (:>))
-import Effectful.Concurrent.STM (Concurrent, TVar)
+import Effectful.Concurrent.STM (TVar)
 import Effectful.Concurrent.STM qualified as STM
-import Effectful.Dispatch.Dynamic (interpret)
+import Effectful.Dispatch.Dynamic (reinterpret)
 import Fun.RequestCounter (RequestCounter (..))
 import Fun.Router qualified as Router
 import UnliftIO qualified as UIO
@@ -14,13 +14,14 @@ import Web.Scotty.Trans (scottyT)
 
 type Counter = TVar Integer
 
-runRequestCounterConcurrent :: (Concurrent :> es) => Counter -> Eff (RequestCounter : es) a -> Eff es a
-runRequestCounterConcurrent counter = interpret $ const \case
-  CurrentCount -> STM.readTVarIO counter
-  IncrementCount -> STM.atomically $ STM.modifyTVar counter (+ 1)
+runRequestCounterConcurrent :: (IOE :> es) => Counter -> Eff (RequestCounter : es) a -> Eff es a
+runRequestCounterConcurrent counter =
+  reinterpret STM.runConcurrent $ const \case
+    CurrentCount -> STM.readTVarIO counter
+    IncrementCount -> STM.atomically $ STM.modifyTVar counter (+ 1)
 
-runIO :: Counter -> Eff '[RequestCounter, Concurrent, IOE] a -> IO a
-runIO counter = runEff . STM.runConcurrent . runRequestCounterConcurrent counter
+runIO :: Counter -> Eff '[RequestCounter, IOE] a -> IO a
+runIO counter = runEff . runRequestCounterConcurrent counter
 
 main :: IO ()
 main = do
